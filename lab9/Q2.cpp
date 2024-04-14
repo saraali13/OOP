67d1086a895a14eb63ea3123c004215f40ca6f3e
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-04-14 14:17:46 +0500

    Update Q2.cpp

--- lab9/Q2.cpp
+++ lab9/Q2.cpp
@@ -2,50 +2,56 @@
 Name:S. Sara Ali
 ID:23K-0070
 */
+
 #include <iostream>
 #include <cmath>
+#include <string>
+using namespace std;
 
-class Shape
+//base class:shape class
+class Shape 
 {
-	private:
-		char shape;
-	public:
-	Shape(char t):shape(t)
-	{}
-	virtual double area() const=0;
-	virtual double perimeter() const=0;
-	virtual void displayProperties () const=0;
-};
+public:
+    virtual ~Shape() 
+    {} // Virtual destructor
 
-class Circle:public Shape
+    //virtual fuctions
+    virtual double area() const = 0;
+    virtual double perimeter() const = 0;
+    virtual void displayProperties() const = 0;
+};//
+
+//derived from shape class
+class Circle : public Shape 
 {
-	private:
+private:
     double radius;
 
 public:
     Circle(double radius) : radius(radius) 
-	{}
+    {}
 
-   virtual double area() const 
-	{
-        return (M_PI * pow(radius, 2));
+    double area() const override 
+    {
+        return M_PI * pow(radius, 2);
     }
 
-    virtual double perimeter() const 
-	{
-        return (2 * M_PI * radius);
+    double perimeter() const override 
+    {
+        return 2 * M_PI * radius;
     }
 
-    virtual void displayProperties() const
-	{
-		cout<<"Displaying data of Circle:\n";
-        cout << "Radius: " << radius <<endl;
+    void displayProperties() const override 
+    {
+        cout << "Displaying data of Circle:\n";
+        cout << "Radius: " << radius << endl;
         cout << "Area: " << this->area() << endl;
-        cout << "Perimeter: " << this->perimeter() <<endl;
+        cout << "Perimeter: " << this->perimeter() << endl;
     }
-};
+};//
 
-class Rectangle:public Shape
+//derived from shape class
+class Rectangle : public Shape 
 {
 private:
     double length;
@@ -53,99 +59,169 @@ private:
 
 public:
     Rectangle(double length, double width) : length(length), width(width) 
-	{}
+    {}
 
-    virtual double area() const override 
-	{
-        return (length * width);
+    double area() const override 
+    {
+        return length * width;
     }
 
-    virtual double perimeter() const override 
-	{
-        return (2 * (length + width));
+    double perimeter() const override 
+    {
+        return 2 * (length + width);
     }
 
-    virtual void displayProperties() const override 
-	{
-		cout<<"Displaying data of Rectangle:\n";
+    void displayProperties() const override {
+        cout << "Displaying data of Rectangle:\n";
         cout << "Length: " << length << endl;
         cout << "Width: " << width << endl;
         cout << "Area: " << this->area() << endl;
         cout << "Perimeter: " << this->perimeter() << endl;
-}
-};
-class Square:public Shape
+    }
+};//
+
+//derived from shape class
+class Square : public Shape 
 {
-	private:
-    double Length;
+private:
+    double sideLength;
 
 public:
-    Square(double Length) : Length(Length) 
-	{}
+    Square(double sideLength) : sideLength(sideLength) 
+    {}
 
-    virtual double area() const  
-	{
+    double area() const override 
+    {
         return pow(sideLength, 2);
     }
 
-    virtual double perimeter() const  
-	{
-        return (4 * sideLength);
+    double perimeter() const override 
+    {
+        return 4 * sideLength;
     }
 
-    virtual void displayProperties() const  
-	{
-		cout<<"Displaying data of Square:\n";
+    void displayProperties() const override 
+    {
+        cout << "Displaying data of Square:\n";
         cout << "Side Length: " << sideLength << endl;
         cout << "Area: " << this->area() << endl;
         cout << "Perimeter: " << this->perimeter() << endl;
     }
 };
-class Triangle:public Shape
+
+//derived from shape class
+class Triangle : public Shape 
 {
-	protected:
+protected:
     double a;
     double b;
     double c;
 
-   public:
-    Triangle(double a, double b, double c) : a(a), b(b), c(c) {}
-    virtual double area() const override 
-	{
+public:
+    Triangle(double a, double b, double c) : a(a), b(b), c(c) 
+    {}
+
+    double area() const override 
+    {
         double s = (a + b + c) / 2;
         return sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
-    virtual double perimeter() const  
-	{
-        return (a + b + c);
+    double perimeter() const override 
+    {
+        return a + b + c;
     }
 
-    virtual void displayProperties() const  
-	{
-		cout<<"Displaying data of Triangle:\n";
-        cout << "Side a: " << a <<endl;
-        cout << "Side b: " << b <<endl;
-        cout << "Side c: " << c <<endl;
-        cout << "Area: " << this->area() <<endl;
-        cout << "Perimeter: " << this->perimeter() <<endl;
+    void displayProperties() const override 
+    {
+        cout << "Displaying data of Triangle:\n";
+        cout << "Side a: " << a << endl;
+        cout << "Side b: " << b << endl;
+        cout << "Side c: " << c << endl;
+        cout << "Area: " << this->area() << endl;
+        cout << "Perimeter: " << this->perimeter() << endl;
     }
-};
-class EquilateralTriangle:public Triangle{
-	public:
-    EquilateralTriangle(double sideLength) : Triangle(sideLength, sideLength, sideLength) 
-	{}
+};//
 
-   virtual void displayProperties() const  
-	{
-	   cout<<"Displaying data of Triangle:\n";
-       cout << "Equilateral Triangle" <<endl;
-       cout << "Side Length: " << a <<endl;
-       cout << "Area: " << this->area() <<endl;
-       cout << "Perimeter: " << this->perimeter() <<endl;
-    
-};
-int main()
+//derived from triangle class
+class EquilateralTriangle : public Triangle 
 {
-	
+public:
+    EquilateralTriangle(double sideLength) : Triangle(sideLength, sideLength, sideLength) 
+    {}
+
+    void displayProperties() const override 
+    {
+        cout << "Displaying data of Equilateral Triangle:\n";
+        cout << "Side Length: " << a << endl;
+        cout << "Area: " << this->area() << endl;
+        cout << "Perimeter: " << this->perimeter() << endl;
+    }
+};//
+
+int main() 
+{
+    cout<<"Name:S.Sara Ali\nID:23k-0070\n\n";
+    
+    string choice;
+    
+    do {
+        cout << "Please select a shape:\n";
+        cout << "1. Circle\n2. Rectangle\n3. Square\n4. Triangle\n";
+        int option;
+        cout << "Enter your choice: ";
+        cin >> option;
+
+        switch (option) 
+        {
+            case 1: 
+            {
+                double radius;
+                cout << "Enter the radius of the circle: ";
+                cin >> radius;
+                Circle circle(radius);
+                circle.displayProperties();
+                break;
+            }
+            case 2: 
+            {
+                double length, width;
+                cout << "Enter the length of the rectangle: ";
+                cin >> length;
+                cout << "Enter the width of the rectangle: ";
+                cin >> width;
+                Rectangle rectangle(length, width);
+                rectangle.displayProperties();
+                break;
+            }
+            case 3: 
+            {
+                double side;
+                cout << "Enter the side length of the square: ";
+                cin >> side;
+                Square square(side);
+                square.displayProperties();
+                break;
+            }
+            case 4: 
+            {
+                double side1, side2, side3;
+                cout << "Enter three side lengths of the triangle: ";
+                cin >> side1 >> side2 >> side3;
+                Triangle triangle(side1, side2, side3);
+                triangle.displayProperties();
+                break;
+            }
+            default:
+                cout << "Invalid choice\n";
+        }
+
+        cout << "Do you want to calculate properties for another shape? (yes/no): ";
+        cin >> choice;
+    } while (choice == "yes");
+
+    cout << "Signing off\n";
+    
+    return 0;
 }
+
